5a3f3becef81cd4bc3b0093275d2172a6b5fb483
sriramteja.ialam@unh.edu <sriramteja.ialam@unh.edu>
2023-12-22 08:07:15 -0500

    Add ajax logic to the views.html

--- src/apps/destinations/api_views.py
+++ src/apps/destinations/api_views.py
@@ -33,4 +33,18 @@ class Countries(TemplateView):
             logging.error(f'Error making API request. Status code: {response.status_code}')
 
         return render(request, 'destinations/countries.html', context={'places': countries_list})
+    
+def ajax_endpoint(request):
+    # Perform any data fetching or processing here
+    # For example, you can make a request to the specified API endpoint
+    api_url = "https://restcountries.com/v3.1/independent?status=true"
+    response = requests.get(api_url)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    if response.status_code == 200:
+        data_ajax = response.json()
+        logging.info(data_ajax)
+        return JsonResponse({'result': data_ajax})
+    else:
+        return JsonResponse({'error': 'Error making API request'}, status=500)
 
